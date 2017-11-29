681c5a2dc6f46c4bab83b57d1ae196c3fc0a3e51
zhangj8 <zhangj8@rose-hulman.edu>
2017-11-29 10:33:14 -0500

    done m6

--- src/m6_summing.py
+++ src/m6_summing.py
@@ -4,8 +4,10 @@ in its simplest classic forms:
    SUMMING:       total = total + number
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Jiatong Zhang.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
+
+import math
 
 
 def main():
@@ -17,7 +19,7 @@ def main():
 def run_test_sum_cosines():
     """ Tests the   sum_cosines   function. """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this function.
+    # DONE: 2. Implement this function.
     #   It TESTS the  sum_cosines  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -28,6 +30,23 @@ def run_test_sum_cosines():
     print('--------------------------------------------------')
     print('Testing the   sum_cosines   function:')
     print('--------------------------------------------------')
+    # Test1
+    expect = 0.13416
+    answer = sum_cosines(3)
+    print('Test1 expected:', expect)
+    print('      actual:', answer)
+
+    #Test2
+    expect = 1
+    answer = sum_cosines(0)
+    print('Test2 expected:', expect)
+    print('      actual', answer)
+
+    #Test3
+    expect = -0.4174477460
+    answer = sum_cosines(10)
+    print('Test3 expected:', expect)
+    print('      actual:', answer)
 
 
 def sum_cosines(n):
@@ -41,19 +60,27 @@ def sum_cosines(n):
         cos(0) + cos(1) + cos(2) + cos(3)   which is about 0.13416.
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
     #   No fair running the code of  sum_cosines  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # ------------------------------------------------------------------
+    total = 1
+    for k in range(n):
+        total = total + math.cos(k+1)
+
+    return total
+
+
+
 
 
 def run_test_sum_square_roots():
     """ Tests the   sum_square_roots   function. """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement this function.
+    # DONE: 4. Implement this function.
     #   It TESTS the  sum_square_roots  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -64,6 +91,23 @@ def run_test_sum_square_roots():
     print('--------------------------------------------------')
     print('Testing the   sum_square_roots   function:')
     print('--------------------------------------------------')
+    # Test 1
+    expected = 3.414213562
+    answer = sum_square_roots(2)
+    print('Test 1 expected:', expected)
+    print('       actual', answer)
+
+    # Test 2
+    expected = 31.77494373
+    answer = sum_square_roots(10)
+    print('Test 2 expected:', expected)
+    print('       actual', answer)
+
+    # Test 3
+    expected = 11.854408
+    answer = sum_square_roots(5)
+    print('Test 3 expected:', expected)
+    print('       actual', answer)
 
 
 def sum_square_roots(n):
@@ -80,13 +124,17 @@ def sum_square_roots(n):
       which is about 11.854408.
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
     #   No fair running the code of  sum_square_roots  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # ------------------------------------------------------------------
+    total = 0
+    for k in range(n):
+        total= total + math.sqrt(2*(k+1))
+    return total
 
 
 # ----------------------------------------------------------------------
